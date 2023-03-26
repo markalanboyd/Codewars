010cb7ab914b4c0281fae3b47e6fcb18617319bc
Mark Boyd <10804241+markalanboyd@users.noreply.github.com>
2023-03-26 16:58:58 +0200

    Added method for getting bishop moves
    It doesn't quite work exactly, but the idea is to get all of the possible moves the bishop can do then compare them against the coordinate where the king currently is. For now it's returning negative coordinates which I need to guard against somehow.

--- python/python_5kyu/Is the King in check ?/main.py
+++ python/python_5kyu/Is the King in check ?/main.py
@@ -14,6 +14,36 @@ def get_coordinates(example_board: list, piece: str) -> tuple:
             return (i, row.index(piece))
     return None
 
+def get_bishop_moves(coord: tuple) -> list:
+    col, row = coord
+    moves = []
+    
+    # Add diagonal positions to the bottom right
+    for i, j in zip(range(row + 1, 8), range(col + 1, 8)):
+        moves.append((i, j))
+        if (i, j) == (7, 7):
+            break
+        
+    # Add diagonal positions to bottom left
+    for i, j in zip(range(row + 1, 8), range(col - 1, 8)):
+        moves.append((i, j))
+        if (i, j) == (7, 0):
+            break
+        
+    # Add diagonal positions to top right
+    for i, j in zip(range(row - 1, 8), range(col + 1, 8)):
+        moves.append((i, j))
+        if (i, j) == (0, 7):
+            break
+    
+    # Add diagonal positions to top left
+    for i, j in zip(range(row - 1, 8), range(col - 1, 8)):
+        moves.append((i, j))
+        if (i, j) == (0, 0):
+            break
+            
+    return moves
+
 def pawn_check(board: list) -> bool:
     for row in board:
         if PAWN not in row:
@@ -30,26 +60,18 @@ def pawn_check(board: list) -> bool:
 def bishop_check(board: list) -> bool:
     king_coord = get_coordinates(board, KING)
     bishop_coord = get_coordinates(board, BISHOP)
-    # (Row, Col)
-    # .....  (0, 5), (0, 7)
-    # Bishop (1, 6)
-    # .....  (2, 5), (2, 7)
-    # King (7, 0)
     king_row = king_coord[0]
     king_col = king_coord[1]
     bishop_row = bishop_coord[0]
     bishop_col = bishop_coord[1]
-    
     if king_row == bishop_row or king_col == bishop_col:
         return False
-    
-        
-    
-    
-        
-        
+    bishop_moves = get_bishop_moves(bishop_coord)
+    print(king_coord)
+    print(bishop_moves)
+    return king_coord in bishop_moves
 
-                
+   
 def is_king_in_check(chessboard: iter) -> bool:
     pawn = next(chessboard)
     pawn_board = parse_board(pawn)

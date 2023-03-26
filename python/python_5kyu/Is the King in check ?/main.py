4fc84fe1cbf2ee55b4f6bee9a251106b99d5bff6
Mark Boyd <10804241+markalanboyd@users.noreply.github.com>
2023-03-26 15:14:34 +0200

    Worked out pawn check detector

--- python/python_5kyu/Is the King in check ?/main.py
+++ python/python_5kyu/Is the King in check ?/main.py
@@ -1,10 +1,33 @@
 import boards
 
-def king_is_in_check(chessboard: list[list[str]]) -> bool:
-    pass
+KING_SYMBOL = '♔'
+PAWN_SYMBOL = '♟'
+
+# Functions
+def parse_board(example_board: dict) -> list:
+    return example_board["board"]
+
+def pawn_check(board: list) -> bool:
+    for row in board:
+        if PAWN_SYMBOL in row:
+            pawn_row = board.index(row)
+            next_row = board[pawn_row + 1]
+            if KING_SYMBOL in next_row:
+                pawn_index = row.index(PAWN_SYMBOL)
+                king_index = next_row.index(KING_SYMBOL)
+                if king_index == (pawn_index - 1) or king_index == (pawn_index + 1):
+                    return True
+                else:
+                    return False
+                
+def is_king_in_check(chessboard: iter) -> bool:
+    pawn = next(chessboard)
+    pawn_board = parse_board(pawn)
+    print(pawn_check(pawn_board))
 
 def main() -> None:
-    king_is_in_check(boards.ITER)
+    is_king_in_check(boards.ITER)
 
+# Main
 if __name__ == "__main__":
     main()

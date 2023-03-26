0636ffa8b8547b7e52988ae5aaea03b51a5b5e32
Mark Boyd <10804241+markalanboyd@users.noreply.github.com>
2023-03-26 15:53:16 +0200

    Refactored pawn_check() and started work on bishop_check()

--- python/python_5kyu/Is the King in check ?/main.py
+++ python/python_5kyu/Is the King in check ?/main.py
@@ -1,29 +1,63 @@
 import boards
 
-KING_SYMBOL = '♔'
-PAWN_SYMBOL = '♟'
+KING = '♔'
+PAWN = '♟'
+BISHOP = '♝'
 
 # Functions
-def parse_board(example_board: dict) -> list:
-    return example_board["board"]
+def parse_board(example: dict) -> list:
+    return example["board"]
+
+def get_coordinates(example_board: list, piece: str) -> tuple:
+    for i, row in enumerate(example_board):
+        if piece in row:
+            return (i, row.index(piece))
+    return None
 
 def pawn_check(board: list) -> bool:
     for row in board:
-        if PAWN_SYMBOL in row:
-            pawn_row = board.index(row)
-            next_row = board[pawn_row + 1]
-            if KING_SYMBOL in next_row:
-                pawn_index = row.index(PAWN_SYMBOL)
-                king_index = next_row.index(KING_SYMBOL)
-                if king_index == (pawn_index - 1) or king_index == (pawn_index + 1):
-                    return True
-                else:
-                    return False
+        if PAWN not in row:
+            continue
+        pawn_row = board.index(row)
+        next_row = board[pawn_row + 1]
+        if KING not in next_row:
+            continue
+        pawn_index = row.index(PAWN)
+        king_index = next_row.index(KING)
+        return king_index == (pawn_index - 1) or king_index == (pawn_index + 1)
+    return False
+
+def bishop_check(board: list) -> bool:
+    king_coord = get_coordinates(board, KING)
+    bishop_coord = get_coordinates(board, BISHOP)
+    # (Row, Col)
+    # .....  (0, 5), (0, 7)
+    # Bishop (1, 6)
+    # .....  (2, 5), (2, 7)
+    # King (7, 0)
+    king_row = king_coord[0]
+    king_col = king_coord[1]
+    bishop_row = bishop_coord[0]
+    bishop_col = bishop_coord[1]
+    
+    if king_row == bishop_row or king_col == bishop_col:
+        return False
+    
+        
+    
+    
+        
+        
+
                 
 def is_king_in_check(chessboard: iter) -> bool:
     pawn = next(chessboard)
     pawn_board = parse_board(pawn)
     print(pawn_check(pawn_board))
+    
+    bishop = next(chessboard)
+    bishop_board = parse_board(bishop)
+    print(bishop_check(bishop_board))
 
 def main() -> None:
     is_king_in_check(boards.ITER)
